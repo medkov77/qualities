7dd8257f13033272b27a3aae6d66902367b26e0b
medkov77 <medkov77@mail.rum>
2022-05-30 20:17:09 +0300

    aga

--- src/app/hooks/useQualities.jsx
+++ src/app/hooks/useQualities.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 
 const QualityContext = React.createContext();
 export const useQualities = () => {
-    return useContext(QualityContext);
+  return useContext(QualityContext);
 };
 const qualities = [{ _id: 2134, name: "Vasya" }];
 export const QualityProvider = ({ children }) => {
-    return (
-        <QualityContext.Provider value={qualities}>
-            {children}
-        </QualityContext.Provider>
-    );
+  return (
+    <QualityContext.Provider value={qualities}>
+      {children}
+    </QualityContext.Provider>
+  );
 };
